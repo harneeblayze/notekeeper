b8884ade05406480771cffe81eedd2e9f40d497e
Harny Otuoniyo <appzone@mrs-MacBook-Pro.local>
2019-04-08 18:22:03 +0100

    stream not working in the right order

--- lib/com/uicontroller/homescreen.dart
+++ lib/com/uicontroller/homescreen.dart
@@ -1,143 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  Container _textItem(BuildContext context, double textsize, Color color,
-      String content, double marginTop, FontWeight fontweight) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: marginTop,
-      ),
-      child: Text(
-        content,
-        style: TextStyle(
-          fontSize: textsize,
-          color: color,
-          fontWeight: fontweight,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildItem(context, DocumentSnapshot document) {
-    return Card(
-      color: Colors.white,
-      margin: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 6),
-      elevation: 2.0,
-      child: new Container(
-          margin: const EdgeInsets.only(left: 10.0),
-          padding: const EdgeInsets.only(top: 9.5, bottom: 0.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _textItem(context, 16.4, Colors.black, document['title'], 5,
-                    FontWeight.bold),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: _textItem(
-                      context,
-                      14,
-                      Colors.grey,
-                      document['body'].length < 30
-                          ? document['body']
-                          : '${document['body'].substring(0, 30)}...',
-                      4,
-                      FontWeight.w300),
-                ),
-                /*RichText(
-                  text: TextSpan(
-                    text: document['title'],
-                    style: new TextStyle(
-                      fontSize: 16.4,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Montserrat",
-                      color: const Color(0xFF5F138D),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: Text(
-                    document['body'],
-                    */ /*document['body'].length < 50
-                              ? document['body']
-                              : '${document['body'].substring(0, 50)}...',*/ /*
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: const Color(0xFF393431),
-                        fontWeight: FontWeight.w300,
-                        fontFamily: "Montserrat"),
-                  ),
-                ),*/
-              ])),
-    );
-  }
-
-  void _showMyBottomSheet(BuildContext context) {
-    // the context of the bottomSheet will be this widget
-    //the context here is where you want to showthe bottom sheet
-    showBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return AddNote(); // returns your BottomSheet widget
-        });
-  }
-
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text('chai'),
-        ),
-        body: StreamBuilder(
-            stream: Firestore.instance.collection('Note').snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return const Text("...Loading");
-              return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) =>
-                      _buildItem(context, snapshot.data.documents[index])
-                  /*NoteItem(snapshot.data.documents[index])*/);
-            }),
-        /*Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                'ball',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          ),
-        ),*/
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _showMyBottomSheet(context);
-          },
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      ),
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class AddNote extends StatefulWidget {
-  @override
-  _AddNoteState createState() => _AddNoteState();
-}
-
-class _AddNoteState extends State<AddNote> {
+class _MyHomePageState extends State<MyHomePage> {
   final _scaffoldKey = new GlobalKey<ScaffoldState>();
   VoidCallback _showPersBottomSheetCallBack;
+  bool removeaAddButton = false;
+  TextEditingController _titleEditingController = new TextEditingController();
+  TextEditingController _bodyEditingController = new TextEditingController();
+  ScrollController _scrollController = new ScrollController();
+  FocusNode _titleFocusNode = new FocusNode();
+  FocusNode _bodyFocusNode = new FocusNode();
+  GlobalKey _titleKey = new GlobalKey();
+  GlobalKey _bodyKey = new GlobalKey();
+  Map<String, dynamic> noteData;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -161,95 +42,175 @@ class _AddNoteState extends State<AddNote> {
       margin: EdgeInsets.only(
         top: marginTop,
       ),
-      child: Center(
-          child: Text(
+      child: Text(
         content,
         style: TextStyle(
           fontSize: textsize,
           color: color,
           fontWeight: fontweight,
         ),
-      )),
+      ),
     );
   }
 
+  Future<void> addNote(noteData) async {
+    Firestore.instance.collection('Note').add(noteData).catchError((e) {
+      print(e);
+    });
+  }
+
 //Button
-  Widget _showButton(String buttonText) {
-    return FlatButton(
-        onPressed: null,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.all(Radius.circular(3.0))),
-          margin: const EdgeInsets.only(top: 11),
-          width: MediaQuery.of(context).size.width / 1.2,
-          height: 56,
-          //color: Colors.green,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                  margin: const EdgeInsets.only(
-                    left: 18,
-                  ),
-                  child: _textItem(context, 16, Colors.white, buttonText, 2,
-                      FontWeight.w400)),
-              Container(
-                margin: const EdgeInsets.only(right: 15),
-                child: ClipOval(
-                  child: Container(
-                    //margin: const EdgeInsets.only(right: 15),
-                    height: 26,
-                    width: 26,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(color: Colors.white, width: 1.0)),
-                    child: IconButton(
-                        padding: const EdgeInsets.only(
-                          top: 0.5,
-                          left: 1.0,
-                        ),
-                        icon: Icon(
-                          Icons.chevron_right,
-                          color: Colors.white,
-                          size: 20,
-                        ),
-                        onPressed: null),
-                  ),
+  Widget _showButton() {
+    return ButtonTheme(
+      minWidth: 200,
+      height: 50,
+      child: new RaisedButton(
+        shape: new RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(30.0),
+        ),
+        child: const Text('Stack Note',
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+        color: Theme.of(context).accentColor,
+        elevation: 4.0,
+        splashColor: Colors.blueGrey,
+        onPressed: () {
+          print('>>>>>>>>>>>>>>>>>>>>>>' + _titleEditingController.text);
+          //Navigator.pop(context);
+          //Navigator.of(context).pop();
+          noteData = {
+            'title': _titleEditingController.text,
+            'body': _bodyEditingController.text
+          };
+          addNote(noteData).then((result) {
+            setState(() {
+              _showPersBottomSheetCallBack = null;
+            });
+            _titleEditingController.clear();
+            _bodyEditingController.clear();
+            SnackBar(
+              content: Text('Note has been added to Stack'),
+              duration: Duration(seconds: 2),
+            );
+          });
+          // Perform some action
+        },
+      ),
+    );
+  }
+
+  void moveToSpecifiedPosition(GlobalKey key) {
+    final RenderBox renderBox = key.currentContext.findRenderObject();
+    final positionRed = renderBox.localToGlobal(Offset(0, 20));
+    _scrollController.animateTo(positionRed.dy,
+        duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+  }
+
+  Widget _buildInputField(
+      TextEditingController controller,
+      FocusNode focusNode,
+      Key key,
+      String hintText,
+      String errorText,
+      int maxLines,
+      double hintSize) {
+    return Container(
+      margin:
+          const EdgeInsets.only(top: 8.5, left: 34.0, right: 33.0, bottom: 8),
+      child: new Theme(
+          data: new ThemeData(
+              fontFamily: 'Montserrat',
+              errorColor: const Color(0xFFFFB822),
+              hintColor: Colors.grey.shade400),
+          child: Container(
+            key: key,
+            child: new TextFormField(
+              controller: controller,
+              focusNode: focusNode,
+              maxLines: maxLines,
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                hintText: hintText,
+                hintStyle: new TextStyle(fontSize: 12.0, color: Colors.white),
+                border: new OutlineInputBorder(
+                  borderRadius: new BorderRadius.circular(4.0),
+                  borderSide:
+                      new BorderSide(width: 0.1, color: Colors.grey.shade100),
                 ),
-              )
-            ],
-          ),
-        ));
+                contentPadding:
+                    const EdgeInsets.only(top: 13.0, bottom: 13.0, left: 8.0),
+              ),
+              style: TextStyle(color: Colors.white),
+              autovalidate: false,
+              validator: (val) {
+                if (val.isEmpty) {
+                  moveToSpecifiedPosition(key);
+
+                  return errorText;
+                }
+              },
+            ),
+          )),
+    );
   }
 
   void _showConfirmQrBottomSheet() {
     setState(() {
       _showPersBottomSheetCallBack = null;
+      removeaAddButton = true;
     });
 
     _scaffoldKey.currentState
         .showBottomSheet((context) {
           Size screenSize = MediaQuery.of(context).size;
           return new Container(
-            height: screenSize.height / 1.2,
+            height: screenSize.height / 1.4,
             decoration: BoxDecoration(
-                color: const Color(0xffffffff),
+                color: Color(0xff36454f),
                 borderRadius: new BorderRadius.only(
                     topLeft: new Radius.circular(11.0),
                     topRight: new Radius.circular(11.0))),
-            child: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
-              _pullDownTray(screenSize),
-              _textItem(context, 20, Color(0xff4a4a4a), "Confirm QRcode", 22,
-                  FontWeight.normal),
-              _textItem(context, 14, Color(0xff9f9f9f),
-                  "Seller should scan to confirm", 6, FontWeight.w500),
-              //Flexible(child: _showBarcodeImage('images/qr_scan.png')),
-              _textItem(context, 24, Color(0xff0d4811), '₦20000', 34,
-                  FontWeight.w700),
-              _showButton("Close")
-            ]),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(child: _pullDownTray(screenSize)),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 34.0),
+                    child: _textItem(context, 12, Colors.white, "Title", 18,
+                        FontWeight.normal),
+                  ),
+                  _buildInputField(
+                      _titleEditingController,
+                      _titleFocusNode,
+                      _titleKey,
+                      'Enter a Title',
+                      'Title cannot be Blank',
+                      1,
+                      12.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 34.0),
+                    child: _textItem(context, 12, Colors.white, "Note Content",
+                        18, FontWeight.normal),
+                  ),
+                  Flexible(
+                    child: _buildInputField(
+                        _bodyEditingController,
+                        _bodyFocusNode,
+                        _bodyKey,
+                        'Type the Note content..',
+                        'please write a note',
+                        12,
+                        12.0),
+                  ),
+
+                  //_formField(
+                  //  10.0, 15.0, 1.0, 'Type a Note', 14.0, _bodyEditingController),
+                  //Flexible(child: _showBarcodeImage('images/qr_scan.png')),
+                  Flexible(child: Center(child: _showButton()))
+                ]),
           );
         })
         .closed
@@ -257,38 +218,77 @@ class _AddNoteState extends State<AddNote> {
           if (mounted) {
             setState(() {
               _showPersBottomSheetCallBack = _showConfirmQrBottomSheet;
+              removeaAddButton = false;
             });
           }
         });
   }
 
+  Widget _buildItem(context, DocumentSnapshot document) {
+    return Card(
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 6),
+      elevation: 5.0,
+      child: new Container(
+          margin: const EdgeInsets.only(left: 10.0),
+          padding: const EdgeInsets.only(top: 9.5, bottom: 0.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: _textItem(context, 16.4, Color(0xff36454f),
+                      document['title'], 5, FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _textItem(
+                      context,
+                      14,
+                      Color(0xff36454f),
+                      document['body'].length < 30
+                          ? document['body']
+                          : '${document['body'].substring(0, 30)}...',
+                      4,
+                      FontWeight.w300),
+                ),
+              ])),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    return Container(
-        child: new Container(
-      height: screenSize.height / 1.2,
-      decoration: BoxDecoration(
-          color: const Color(0xffffffff),
-          borderRadius: new BorderRadius.only(
-              topLeft: new Radius.circular(11.0),
-              topRight: new Radius.circular(11.0))),
-      child: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
-        _pullDownTray(screenSize),
-        _textItem(context, 20, Color(0xff4a4a4a), "Confirm QRcode", 22,
-            FontWeight.normal),
-        _textItem(context, 14, Color(0xff9f9f9f),
-            "Seller should scan to confirm", 6, FontWeight.w500),
-        //Flexible(child: _showBarcodeImage('images/qr_scan.png')),
-        _textItem(
-            context, 24, Color(0xff0d4811), '₦20000', 34, FontWeight.w700),
-        _showButton("Close")
-      ]),
-    ));
-    /*floatingActionButton: FloatingActionButton(
-        onPressed: _showPersBottomSheetCallBack,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),*/
+    return MaterialApp(
+      theme: ThemeData(canvasColor: Colors.transparent),
+      home: Scaffold(
+        //resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        key: _scaffoldKey,
+        appBar: AppBar(
+          //backgroundColor: Color(0xff36454f),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Note Keeper'),
+        ),
+        body: StreamBuilder(
+            stream: Firestore.instance.collection('Note').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text("...Loading");
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) =>
+                      _buildItem(context, snapshot.data.documents[index])
+                  /*NoteItem(snapshot.data.documents[index])*/);
+            }),
+        floatingActionButton: removeaAddButton
+            ? Container()
+            : FloatingActionButton(
+                onPressed: _showPersBottomSheetCallBack,
+                tooltip: 'Add Note',
+                child: Icon(Icons.add),
+              ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
   }
 }
